d90e76c0173550e870d82d24f37078e6de07273f
Alexis Barreirinho <alexis@barreirinho.com>
2025-08-23 15:57:25 +0200

    add state machine to player

---
+++ Player/scripts/state_idle.gd
@@ -0,0 +1,23 @@
+class_name StateIdle extends State
+
+@onready var walk: State = $"../Walk"
+
+
+func enter() -> void:
+	player.update_animation("idle")
+	pass
+
+func exit() -> void:
+	pass
+
+func process(_delta: float) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity = Vector2.ZERO
+	return null
+
+func physics(_delta: float) -> State:
+	return null
+	
+func handle_input(_event: InputEvent) -> State:
+	return null
